4228a4e34a01a062545b8a356be86b05e6ee4804
mumostevn <ndundastevn.makueni.go.ke>
2024-07-12 14:52:26 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/mumostevn.github.io",
+	base: "/mumostevn.github.io/",
   plugins: [vue()],
 })
